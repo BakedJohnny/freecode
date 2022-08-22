4abfce28256db13b2598da7203e56b4e74d593f3
BakedJohnny <johnkjays98@gmail.com>
2022-08-22 10:26:50 +0530

    almost done

--- javascript/script.js
+++ javascript/script.js
@@ -15,3 +15,14 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// $(document).ready(function() {
+//   $('#close-btn').click(function() {
+//     $('#search-overlay').fadeOut();
+//     $('#search-btn').show();
+//   });
+//   $('#search-btn').click(function() {
+//     $(this).hide();
+//     $('#search-overlay').fadeIn();
+//   });
+// });
