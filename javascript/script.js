8c8085ac19edab125ee38f87af1a134c78e88347
BakedJohnny <johnkjays98@gmail.com>
2022-08-22 14:18:44 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -15,14 +15,3 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-
-// $(document).ready(function() {
-//   $('#close-btn').click(function() {
-//     $('#search-overlay').fadeOut();
-//     $('#search-btn').show();
-//   });
-//   $('#search-btn').click(function() {
-//     $(this).hide();
-//     $('#search-overlay').fadeIn();
-//   });
-// });
